8a1fb8595bf2869793cab4e110b88f2902ef13ac
Zac Smith <zrs3141592@gmail.com>
2021-01-17 18:28:31 -0700

    fixes #1

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Auth0Provider } from '@auth0/auth0-spa-js'
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './src/App'
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 render(
-    <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        redirectUri={window.location.origin}
-    >
-        <App />
-    </Auth0Provider>,
-    document.querySelector('#root')
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.querySelector('#root')
 )
