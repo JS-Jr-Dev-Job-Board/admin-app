8cf27c53682b70d1bebd37eede3f5bd45f88b79c
BrianReisman <brianreisman@protonmail.com>
2021-01-17 22:44:17 -0500

    created card for each result of crawl/api call

--- src/components/EndpointPage/EndpointPage.js
+++ src/components/EndpointPage/EndpointPage.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PotentialJob from "./PotentialJob/PotentialJob";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const dummydata = [
   {
@@ -9,6 +9,7 @@ const dummydata = [
     source: "craigslist.com",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod explicabo officiis corporis minus totam commodi, id debitis neque consequuntur alias repudiandae distinctio nihil iure quae a laboriosam labore obcaecati dolore vel. Asperiores facilis amet ad incidunt iste laudantium delectus itaque inventore sed voluptates similique est cumque ex reprehenderit, dolor magnam excepturi. Nobis voluptatum harum iure.",
+    title: "Jr. JavaScript Developer",
   },
   {
     company: "b",
@@ -16,6 +17,7 @@ const dummydata = [
     source: "linkedin.com",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod explicabo officiis corporis minus totam commodi, id debitis neque consequuntur alias repudiandae distinctio nihil iure quae a laboriosam labore obcaecati dolore vel. Asperiores facilis amet ad incidunt iste laudantium delectus itaque inventore sed voluptates similique est cumque ex reprehenderit, dolor magnam excepturi. Nobis voluptatum harum iure.",
+    title: "Jr. JavaScript Developer",
   },
   {
     company: "c",
@@ -23,6 +25,7 @@ const dummydata = [
     source: "monster.com",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod explicabo officiis corporis minus totam commodi, id debitis neque consequuntur alias repudiandae distinctio nihil iure quae a laboriosam labore obcaecati dolore vel. Asperiores facilis amet ad incidunt iste laudantium delectus itaque inventore sed voluptates similique est cumque ex reprehenderit, dolor magnam excepturi. Nobis voluptatum harum iure.",
+    title: "Jr. JavaScript Developer",
   },
   {
     company: "d",
@@ -30,29 +33,51 @@ const dummydata = [
     source: "angel.co",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod explicabo officiis corporis minus totam commodi, id debitis neque consequuntur alias repudiandae distinctio nihil iure quae a laboriosam labore obcaecati dolore vel. Asperiores facilis amet ad incidunt iste laudantium delectus itaque inventore sed voluptates similique est cumque ex reprehenderit, dolor magnam excepturi. Nobis voluptatum harum iure.",
+    title: "Jr. JavaScript Developer",
   },
 ];
 
 const EndpointPage = (props) => {
-  const [selectedListings, setSelectedListings] = useState();
+  const [selectedListings, setSelectedListings] = useState({});
+  console.log(selectedListings);
 
   const selectedJobHandler = (e) => {
+    setSelectedListings({ ...selectedListings, [e.target.id]: "approved" });
+    console.log(e.target);
+  };
 
-  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    alert("You have selected the following jobs:", selectedListings);
+  };
 
   const potentialJobs = dummydata.map((job) => {
-    return <PotentialJob data={job} key={uuidv4()}/>;
+    return (
+      <PotentialJob
+        data={job}
+        key={uuidv4()}
+        id={job.company}
+        onChange={selectedJobHandler}
+        checked={false}
+      />
+    );
   });
-
 
   return (
     <div>
-      <div className="top">
+      <div className="EndpointPageTop">
         <h1>GitHub</h1> {/*props.site*/}
-        <button>FIRE AWAY!</button>
+        <button className="EndpointPageFireBtn">FIRE AWAY!</button>
       </div>
-      <div className="potentialListings"></div>
-      {potentialJobs}
+      <form className="EndpointPagePotentialListings" onSubmit={submitHandler}>
+        {potentialJobs}
+
+        <div className="EndpointPageFooter">
+          <button className="EndpointPageSubmitBtn" >
+            Add selected jobs to database
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/EndpointPage/PotentialJob/PotentialJob.js
+++ src/components/EndpointPage/PotentialJob/PotentialJob.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 const PotentialJob = props => {
-  const {company, salary, description, source} = props.data;
+  const {company, salary, description, source, title} = props.data;
 console.log(props.asdf)
   return(
-    <div>
-      <p>{company}</p>
-      <p>{source}</p>
-      <p>{salary}</p>
-      <p>{description}</p>
-      <input type="checkbox" name="approve" id="approve"/>
+    <div className='PotentialJobCard'>
+      <p>Company: {company}</p>
+      <p>Job Title: {title}</p>
+      <p>Source: {source}</p>
+      <p>Salary: {salary}</p>
+      <p>Description: {description}</p>
+      <input type="checkbox" name="approve" id={props.id} onChange={props.onChange} checked={props.checked}/>
     </div>
   )
 }
