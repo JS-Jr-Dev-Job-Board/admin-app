7a3058b84442ddcf064fa8133c9549527829a20a
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2021-01-18 21:46:13 -0500

    top margin

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import DashboardCard from "./DashboardCard";
 
 const Dashboard = ({}) => {
   return (
-    <div>
+    <div className="dashboard">
       <h1>Admin Dashboard</h1>
 
       <DashboardCard />
